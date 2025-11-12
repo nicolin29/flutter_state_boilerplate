001b0c105ef69b9988ae5d4c897ff3de2dc0e65a
Nicodemus <nicodemusdemuse@gmail.com>
2025-11-12 11:55:06 +0700

    infinite list done

--- lib/src/feature/article/presentation/01_bloc/page/article_list_page.dart
+++ lib/src/feature/article/presentation/01_bloc/page/article_list_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_state_boilerplate/src/feature/article/presentation/01_bloc/bloc/article_list/article_list_bloc.dart';
+import 'package:flutter_state_boilerplate/src/feature/article/presentation/01_bloc/bloc/article_list/article_list_event.dart';
+import 'package:flutter_state_boilerplate/src/feature/article/presentation/01_bloc/bloc/article_list/article_list_state.dart';
 
 class ArticleListPage extends StatefulWidget {
   static const String routeName = '/article-list';
@@ -11,8 +15,55 @@ class ArticleListPage extends StatefulWidget {
 }
 
 class _ArticleListPageState extends State<ArticleListPage> {
+  late final ArticleListBloc _bloc;
+
+  @override
+  void initState() {
+    _bloc = context.read<ArticleListBloc>();
+    _bloc.add(FetchArticles());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Article List Page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Article List Page')),
+      body: BlocBuilder<ArticleListBloc, ArticleListState>(
+        builder: (context, state) {
+          if (state.isLoading && state.articles.isEmpty) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (state.error != null && state.articles.isEmpty) {
+            return Center(child: Text(state.error!));
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (scrollInfo) {
+              if (scrollInfo.metrics.pixels >=
+                  scrollInfo.metrics.maxScrollExtent - 200.0) {
+                _bloc.add(LoadMore());
+              }
+              return false;
+            },
+            child: ListView.builder(
+              itemCount: state.articles.length + (state.hasMore ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index < state.articles.length) {
+                  return ListTile(
+                    title: Text(state.articles[index].title ?? '-'),
+                  );
+                } else {
+                  return Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
